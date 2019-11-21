14cfc9d06503fadfd78405a34b3aac354fb62d3b
lynx <2104934038@qq.com>
2019-11-21 08:59:33 +0800

    修改分支

--- my_update.sh
+++ my_update.sh
@@ -1,4 +1,5 @@
-echo "\033[1;34mAdding...\033[0m"
+branch=`git symbolic-ref --short -q HEAD`
+echo "\033[1;34mAdding to ${branch}\033[0m"
 if [ ${#1} -le 0 ]
 then
   echo "\033[1;33mNeed Message\033[0m"
@@ -8,7 +9,7 @@ else
     echo "\033[1;32mPushing...\033[0m"
   if [ ${#2} -ge 1 ]
   then
-    git push origin master
+    git push origin ${branch}
   else
     echo "\033[1;35mPush Abort\033[0m"
   fi
