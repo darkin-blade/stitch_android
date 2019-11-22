849247de2d679460fae42b92d3950d251966291d
lynx <2104934038@qq.com>
2019-11-22 16:50:03 +0800

    todo:移植opencv

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -43,7 +43,6 @@ Java_com_example_stitch_MainActivity_getImg(
     AndroidBitmapInfo info;
     void *pixels = 0;
 
-    LOG("nMatToBitmap");
     CV_Assert(AndroidBitmap_getInfo(env, img_send, &info) >= 0);
     CV_Assert(info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
               info.format == ANDROID_BITMAP_FORMAT_RGB_565);
@@ -52,15 +51,17 @@ Java_com_example_stitch_MainActivity_getImg(
     CV_Assert(src.type() == CV_8UC1 || src.type() == CV_8UC3 || src.type() == CV_8UC4);
     CV_Assert(AndroidBitmap_lockPixels(env, img_send, &pixels) >= 0);
     CV_Assert(pixels);
+
     if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        LOG("8888 format");
         Mat tmp(info.height, info.width, CV_8UC4, pixels);
         if (src.type() == CV_8UC1) {
             LOG("nMatToBitmap: CV_8UC1 -> RGBA_8888");
             cvtColor(src, tmp, COLOR_GRAY2RGBA);
         } else if (src.type() == CV_8UC3) {
             LOG("nMatToBitmap: CV_8UC3 -> RGBA_8888");
-            cvtColor(src, tmp, COLOR_RGB2RGBA);
-        } else if (src.type() == CV_8UC4) {
+            cvtColor(src, tmp, COLOR_BGR2RGBA);
+        } else if (src.type() == CV_8UC4) {// TODO BGR
             LOG("nMatToBitmap: CV_8UC4 -> RGBA_8888");
             if (true)  // needPremultiplyAlpha: TODO 是否前乘透明度
                 cvtColor(src, tmp, COLOR_RGBA2mRGBA);
@@ -68,22 +69,21 @@ Java_com_example_stitch_MainActivity_getImg(
                 src.copyTo(tmp);
         }
     } else {
-        // info.format == ANDROID_BITMAP_FORMAT_RGB_565
+        LOG("%d", info.format == ANDROID_BITMAP_FORMAT_RGB_565);// TODO
         Mat tmp(info.height, info.width, CV_8UC2, pixels);
         if (src.type() == CV_8UC1) {
             LOG("nMatToBitmap: CV_8UC1 -> RGB_565");
             cvtColor(src, tmp, COLOR_GRAY2BGR565);
-        } else if (src.type() == CV_8UC3) {
+        } else if (src.type() == CV_8UC3) {// TODO BGR
             LOG("nMatToBitmap: CV_8UC3 -> RGB_565");
             cvtColor(src, tmp, COLOR_RGB2BGR565);
-        } else if (src.type() == CV_8UC4) {
+        } else if (src.type() == CV_8UC4) {// TODO BGR
             LOG("nMatToBitmap: CV_8UC4 -> RGB_565");
             cvtColor(src, tmp, COLOR_RGBA2BGR565);
         }
     }
 
     AndroidBitmap_unlockPixels(env, img_send);
-    return;
 }
 
 JNIEXPORT void JNICALL
